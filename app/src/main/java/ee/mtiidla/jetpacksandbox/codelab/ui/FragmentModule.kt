26edf815017e6f301abc86ad65e20524b874df27
Marko Tiidla <marko.tiidla@underarmour.com>
2018-10-10 11:28:09 +0200

    Add explanation of this solution

--- app/src/main/java/ee/mtiidla/jetpacksandbox/codelab/ui/FragmentModule.kt
+++ app/src/main/java/ee/mtiidla/jetpacksandbox/codelab/ui/FragmentModule.kt
@@ -20,6 +20,14 @@ abstract class FragmentModule {
         @Provides
         fun provideArgument(bundle: Bundle) : Any = bundle.getParcelable("arg")!!
 
+        // To provide parameters to ViewModels per VM, a component and module is required for each
+        // fragment, however that approach would require a lot of boilerplate to write the components
+        // and modules. A solution would be piggyback on top of fragment arguments, so keep a single
+        // FragmentModule and FragmentComponent but expose its arguments for resolving VM parameters
+
+        // Ofc this module becomes a god object and a confusing mess
+        // Here could be a list of all the parameters for all the fragments cast to the correct type
+
         @JvmStatic
         @Provides
         fun provideWordDetailScreenArgument(argument: Any) : WordDetailScreenArg = argument as WordDetailScreenArg
