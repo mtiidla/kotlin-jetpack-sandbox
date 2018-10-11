33ee7ef7efe6ecd2ab44a69ed68219cfc875b953
Marko Tiidla <marko.tiidla@underarmour.com>
2018-10-11 09:32:55 +0200

    Add solution for injecting vm parameters by creating a component for each vm

--- app/src/main/java/ee/mtiidla/jetpacksandbox/codelab/ui/detail/WordDetailComponent.kt
+++ app/src/main/java/ee/mtiidla/jetpacksandbox/codelab/ui/detail/WordDetailComponent.kt
@@ -1,12 +1,30 @@
 package ee.mtiidla.jetpacksandbox.codelab.ui.detail
 
+import dagger.BindsInstance
 import dagger.Subcomponent
 
-@Subcomponent(
-        modules = arrayOf(WordDetailModule::class)
-)
+@Subcomponent
 interface WordDetailComponent {
 
+    /**
+     * This solution removes the need for creating a module for every single fragment that requires
+     * parameters, but still requires creating a component for every fragment. It provides type
+     * safety for the parameters as each component can extract specific fragment arguments to forward
+     * them to the VMs.
+     *
+     * While it allows decoupling, it can't be used to inject fragments in a generic and global way
+     * e.g. using global fragment lifecycle callbacks.
+     */
+
     fun inject(fragment: WordDetailFragment)
+
+    @Subcomponent.Builder
+    interface Builder {
+
+        @BindsInstance
+        fun arguments(arguments: WordDetailScreenArg): Builder
+
+        fun build(): WordDetailComponent
+    }
 
 }
